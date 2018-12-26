10331f98eb2aaf5d2c6dd89491796e17aecf001d
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-12-26 17:32:12 +0530

    Initial commit

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+
+class AuthorQuiz extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Author Quiz</h1>
+      </div>
+    );
+  }
+}
+
+export default AuthorQuiz;
