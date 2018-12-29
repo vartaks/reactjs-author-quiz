9bcbe0816d56ca397237287723532cb26a27b450
Sourabh Vartak <sourabh.vartak@cimpress.com>
2018-12-29 15:10:13 +0530

    Converted the Add Author part of the application to use React Redux store

--- src/AddAuthorForm.js
+++ src/AddAuthorForm.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import "./AddAuthorForm.css";
 
 class AuthorForm extends Component {
@@ -57,4 +59,13 @@ function AddAuthorForm({ match, onAddAuthor }) {
     </div>;
 }
 
-export default AddAuthorForm;
+function mapDispatchToProps(dispatch, props) {
+    return {
+        onAddAuthor: (author) => {
+            dispatch({ type: 'ADD_AUTHOR', author });
+            props.history.push('/');
+        }
+    };
+}
+
+export default withRouter(connect(() => {}, mapDispatchToProps)(AddAuthorForm));
